476ef983469f2b1c1e5a8550e9bceefe6eb0a9f1
danny <danny@korealaundry.com>
2021-06-17 09:43:50 +0900

    BlockingQueue

--- android/app/src/main/java/com/yourcompany/event_channel_sample/MainActivity.java
+++ android/app/src/main/java/com/yourcompany/event_channel_sample/MainActivity.java
@@ -1,25 +1,70 @@
 package com.yourcompany.event_channel_sample;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends FlutterActivity {
     public static final String TAG = "eventchannelsample";
     public static final String STREAM = "com.korealaundry.eventchannelsample/stream";
+    public static final String CHANNEL = "com.korealaundry.eventchannelsample/channel";
 
     private Disposable timerSubscription;
+    private BlockingQueue<Map<String, Object>> blockingQueue = new BlockingQueue(100);
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this.getFlutterEngine());
+        new MethodChannel(this.getFlutterEngine().getDartExecutor(), CHANNEL).setMethodCallHandler(
+                new MethodChannel.MethodCallHandler() {
+                    @Override
+                    public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+
+                        if (call.method.equals("addQueue")) {
+                            Log.w(TAG, "=== addQueue");
+                            Map<String, Object> data = new HashMap();
+                            data.put("test", "test");
+                            try {
+                                blockingQueue.enqueue(data);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            result.notImplemented();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        //// TODO: 2017-07-03
+        String test = intent.getStringExtra("test");
+        Map<String, Object> data = new HashMap();
+        data.put("test", test);
+        try {
+            blockingQueue.enqueue(data);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d("테스트", test);
+    }
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
@@ -30,6 +75,12 @@ public class MainActivity extends FlutterActivity {
                     @Override
                     public void onListen(Object args, EventChannel.EventSink events) {
                         Log.w(TAG, "adding listener");
+                        try {
+                            events.success(blockingQueue.dequeue());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        /*
                         timerSubscription = Observable
                                 .interval(0, 1, TimeUnit.SECONDS)
                                 .observeOn(AndroidSchedulers.mainThread())
@@ -44,6 +95,8 @@ public class MainActivity extends FlutterActivity {
                                         },
                                         () -> Log.w(TAG, "closing the timer observable")
                                 );
+
+                         */
                     }
 
                     @Override
